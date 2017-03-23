4fc327a499fe57574982fee11607de66b6ec027b
luwel <luwel@esquel.com>
2017-03-23 09:42:39 +0800

    带权限

--- FNM/Client/替换版本.bat
+++ FNM/Client/替换版本.bat
@@ -1,5 +1,25 @@
 @echo off&SetLocal EnableDelayedExpansion
 
+
+:: Get ADMIN Privs
+:-------------------------------------
+mkdir "%windir%\BatchGotAdmin"
+if '%errorlevel%' == '0' (
+  rmdir "%windir%\BatchGotAdmin" & goto gotAdmin 
+) else ( goto UACPrompt )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/s /c " ^& """"%0"""" ^& " ", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"      
+    CD /D "%~dp0"
+:-------------------------------------
+:: End Get ADMIN Privs
+
+
 del FNM\Client\FNMAutoTemp.rc
 
 for /f "skip=1 delims=" %%i in (FNM\Client\fnmauto.rc) do ( 
